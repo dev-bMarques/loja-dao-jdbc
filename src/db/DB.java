86ce596d4cd110fdbc89349ba5eb4d315da6523e
Bruno Marques <dev.b.marques@gmail.com>
2026-01-08 21:18:53 -0300

    Versão do projeto com aplicação do banco de dados SQL

--- src/db/DB.java
+++ src/db/DB.java
@@ -52,22 +52,19 @@ public class DB {
 		if (st != null) {
 			try {
 				st.close();
-			} 
-			catch (SQLException e) {
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
-			}
-			}
-			}
-			
-	public static void closeResultSet(ResultSet rs) {
-				if (rs != null) {
-					try {
-						rs.close();
-					} 
-					catch (SQLException e) {
-						throw new DbException(e.getMessage());
 			}
 		}
 	}
 
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
 }
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,15 +1,16 @@
 package model.dao;
+
 import java.util.List;
 
+import model.entities.Department;
 import model.entities.Seller;
 
-	public interface SellerDao {
+public interface SellerDao {
 
-		void insert (Seller obj);
-		void update (Seller obj);
-		void deleteById(Seller id);
-		Seller findById (Integer id);
-		List <Seller> findAll();
-	}
-
-
+	void insert(Seller obj);
+	void update(Seller obj);
+	void deleteById(Integer id);
+	Seller findById(Integer id);
+	List<Seller> findAll();
+	List<Seller> findByDepartment(Department department);
+}
